915486b2cb96a6300e1492c92ec71010b26431e6
Nicolas Graves <ngraves@ngraves.fr>
2024-11-05 14:36:32 +0100

    wb_wdi: Enable codelist and datastructure.

--- sdmx/source/wb_wdi.py
+++ sdmx/source/wb_wdi.py
@@ -15,6 +15,16 @@ class Source(BaseSource):
         elif kwargs.get("resource_type") == Resource.data:
             # Provider's own ID differs from its ID in this package
             kwargs.setdefault("provider", "WB")
+        elif kwargs.get("resource_type") in {Resource.datastructure, Resource.codelist}:
+            # Here /all/latest is not allowed.
+            # It is added automatically, use url to circumvent that.
+            name = kwargs.get("resource_type").name
+            kwargs.pop("resource_type")
+            if not all(value is None for value in kwargs.values()):
+                raise ValueError(
+                    f"WDI {name} is a unique endpoint and doesn't support arguments"
+                )
+            kwargs.setdefault("url", f"https://api.worldbank.org/v2/sdmx/rest/{name}/wb")
 
         try:
             if isinstance(kwargs["key"], str):
